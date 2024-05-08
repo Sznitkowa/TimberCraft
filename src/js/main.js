e1f5ee11ea1ccb3df682a89f5f601aa8466c528f
Agnieszka <agnieszka.jaczewska.97@gmail.com>
2024-05-08 16:05:32 +0200

    finished site

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ const footerYear = document.querySelector('.footer__year');
 const navMobileBox = document.querySelector('.nav-mobile__box');
 const burgerBtn = document.querySelector('.nav-mobile__btn--burger');
 const closeBtn = document.querySelector('.nav-mobile__btn--close');
+const allNavLinks = document.querySelectorAll('.nav__link--mobile');
 
 const handleFooterYear = () => {
 	const year = new Date().getFullYear();
@@ -11,6 +12,12 @@ const handleFooterYear = () => {
 const toggleMenu = () => {
 	navMobileBox.classList.toggle('show-menu');
 };
+
+allNavLinks.forEach((link) => {
+	link.addEventListener('click', () => {
+		navMobileBox.classList.remove('show-menu');
+	});
+});
 
 handleFooterYear();
 burgerBtn.addEventListener('click', toggleMenu);
